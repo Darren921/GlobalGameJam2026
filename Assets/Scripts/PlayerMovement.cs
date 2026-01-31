4b30ce61bfb1ff0b008d42a367639474ca73a0d6
N1c1an <nicolas.chu@ontariotechu.net>
2026-01-31 18:32:25 -0500

    Stage 2 updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -58,7 +58,7 @@ public class PlayerMovement : MonoBehaviour
     
     private void Update()
     {
-//        print(player.Rb.linearVelocity);
+        print(player.Rb.linearVelocity);
     }
 
     private void FixedUpdate()
